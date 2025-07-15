d0aa10748ebf38e20dd201fb88e1762cd18acf8e
knyarku25 <knyarku@luc.edu>
2025-07-15 16:59:28 +0000

    Completed

--- SuperAles.py
+++ SuperAles.py
@@ -1,4 +1,32 @@
+class Ale:
+    def __init__(self, name, abv):
+        self.name = name
+        self.abv = abv
 
+def alcohol_content(self, volunme_in_oz):
+    return self.abv * volunme_in_oz
+
+def description(self):
+    return f"(self.name): {self.abv * 100:.1f}% ABV"
+
+class PaleAle(Ale):
+    def __init__(self):
+        super().__init__("Pale Ale", 0.055)
+
+
+class IPA(Ale):
+    def __init__(self):
+        super().__init__("IPA", 0.065)
+
+
+class Stout(Ale):
+    def __init__(self):
+        super().__init__("Stout", 0.07)
+
+
+class Porter(Ale):
+    def __init__(self):
+        super().__init__("Porter", 0.06)
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
--- SuperVehicles.py
+++ SuperVehicles.py
@@ -1,4 +1,37 @@
+# Base class for all vehicles
+class Vehicle:
+    def __init__(self, name, mpg):
+    # Set the name and mpg (miles per gallon) for the vehicle 
+        self.name = name
+        self.mpg = mpg  # miles per gallon
 
+    def fuel_needed(self, distance):
+         # Calculates how much fuel is needed for a given distance
+        result =  distance / self.mpg
+        return result
+
+    def description(self):
+        return f"{self.name} gets {self.mpg} miles per gallon."
+
+
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__("Car", 30)
+
+
+class Truck(Vehicle):
+    def __init__(self):
+        super().__init__("Truck", 15)
+
+
+class Motorcycle(Vehicle):
+    def __init__(self):
+        super().__init__("Motorcycle", 50)
+
+
+class Bus(Vehicle):
+    def __init__(self):
+        super().__init__("Bus", 8)
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
